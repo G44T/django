4bb8f6ab747d70a5b625b6cefa4b7613ddcc7298
Gianfranco <gianfrancoarnaldo111@gmail.com>
2025-02-18 00:23:01 -0500

    Django con view

---
+++ inicio/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from inicio.views import inicio, saludo
+
+urlpatterns = [
+    path('',inicio, name='inicio'),
+    path('saludo/<str:nombre>/<str:apellido>', saludo, name='saludo')
+]
